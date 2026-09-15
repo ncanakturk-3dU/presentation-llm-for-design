333c7b14a44725b7f264ce57baf90d7393409d24
N Can Akturk <nuri@3duniversum.com>
2026-09-15 22:13:54 +0200

    Add Vite React project scaffold and config

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './design/fonts.css'
+import './design/tokens.css'
+import './design/base.css'
+
+createRoot(document.getElementById('root')).render(<App />)
